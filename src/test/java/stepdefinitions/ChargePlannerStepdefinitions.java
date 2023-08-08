12bc57a26b38c956e86d598589275dd870af9b44
Muhammed Furkan Kocer <muhammedfurkankocer@gmail.com>
2023-08-09 00:05:13 +0300

    f

--- src/test/java/stepdefinitions/ChargePlannerStepdefinitions.java
+++ src/test/java/stepdefinitions/ChargePlannerStepdefinitions.java
@@ -1,4 +1,11 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.Then;
+
 public class ChargePlannerStepdefinitions {
+
+    @Then("Hello world")
+    public void hello_world() {
+        System.out.println("hi!");
+    }
 }
